ca73465b6ceb3c8555976ce29c6930983b3043c1
«EkaterinaKopylova» <ekaterina.sunday@gmail.com>
2021-07-09 18:10:28 +0300

    flex

--- maket-flex/script.js
+++ maket-flex/script.js
@@ -2,3 +2,23 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 
 }
+
+function openMenu(){
+    var sidemenu = document.getElementById("side");
+    sidemenu.style.display='block';
+}
+function hideMenu(){
+    var sidemenu = document.getElementById("side");
+    sidemenu.style.display='none';
+}
+function openAddSideMenu(){
+    var addsidemenu = document.getElementById("addsidemenu");
+    addsidemenu.style.display="block";
+}
+function closeAddSideMenu(){
+    var closeaddsidemenu = document.getElementById('closeaddsidemenu');
+    var sidemenu = document.getElementById("side");
+    sidemenu.style.display='none';
+    addsidemenu.style.display='none';
+    
+}
