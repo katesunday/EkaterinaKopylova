2330cf90b2afc6e0fba859db8837836c2726841a
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2021-08-22 19:15:13 +0300

    fd2

---
+++ FD2/lesson5/hashStorage.js
@@ -0,0 +1,67 @@
+class HashStorage{
+    constructor(){
+        this.storage_ = {};
+    }
+    addValue(key,value){
+        this.storage_[key] = value;
+        
+    }
+    getValue(key){
+        console.log(this.storage_[key])
+      
+        return this.storage_[key];
+    }
+    deleteValue(key){
+        if( !(key in this.storage_)){
+            alert('Нет такого напитка')
+            return false;
+            
+        }
+        else{
+            delete this.storage_[key];
+            alert(`Коктейль "${key}" удален!`)
+            return true;
+        }
+    }
+    getKeys(){
+        console.log(Object.keys(this.storage_));
+        return Object.keys(this.storage_);
+        
+    }
+}
+let coctailsStorage = new HashStorage();
+ 
+coctailsStorage.addValue("Маргарита",{ алкогольный: 'да',
+ ингридиенты: 'Водка Finlandia 50мл\nКофейный ликер 25мл \n Лед в кубиках 120г',
+рецепт: 'Наполни стакан кубиками льда доверху, затем налей кофейный ликер 25 мл, водку 50 мл и размешай коктейльной ложкой.'});
+
+coctailsStorage.addValue('Пеликан',{алкогольный: "да",
+ингридиенты: "Гренадин Monin 10мл\nКлубничный сироп Monin 10мл\nПерсиковый сок 150мл\nЛимонный сок 15мл\nБанан 110г\nКлубника 50г\nДробленый лед 60г",
+рецепт: 'Положи в блендер очищенную и нарезанную половинку банана и клубнику 2 ягоды. Налей лимонный сок 15 мл, гренадин 10 мл, клубничный сироп 10 мл и персиковый сок 150 мл. Добавь в блендер совок дробленого льда и взбей. Перелей в хайбол. Укрась кружком банана и половинкой клубники на коктейльной шпажке.'
+});
+
+function delCoctail(){
+    var name = prompt('Введите название коктейля', "Маргарита");
+   
+   coctailsStorage.deleteValue(name);
+}
+function addRecipe(){
+    var name = prompt('Введите название напитка.', "Имбирный чай");
+    var isalcohol = prompt('Напиток алкогольный?', "нет");
+    var ingridients = prompt('Перечислите ингридиенты.');
+    var recipe = prompt('Напишите рецепт приготовления.')
+
+    coctailsStorage.addValue(name,{алкогольный: isalcohol,
+    ингридиенты: ingridients,
+    рецепт: recipe,});
+    alert(`Напиток "${name}" добавлен!`);
+}
+function showCoctail(){
+    var name = prompt('Какой напиток хотите найти?', "Маргарита");
+    coctailsStorage.getValue(name);
+    
+}
+function allRecipies(){
+    coctailsStorage.getKeys();
+    
+}
