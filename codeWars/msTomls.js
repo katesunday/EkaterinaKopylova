45d7280f856b35c6341351f1ce4f167b31f5e16c
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2022-01-18 13:58:05 +0400

    cw

---
+++ codeWars/msTomls.js
@@ -0,0 +1,6 @@
+function past(h, m, s){
+    const convertHours = h * 60 * 60 * 1000;
+    const convertMinutes = m * 60 * 1000;
+    const convertSeconds = s * 1000;
+    return convertHours + convertMinutes + convertSeconds;
+    }
