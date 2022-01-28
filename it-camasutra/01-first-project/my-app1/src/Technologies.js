843763e3e2cb7a285e5835cf1acdfd5bbc2585f3
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2022-01-28 17:09:17 +0400

    it-c

---
+++ it-camasutra/01-first-project/my-app1/src/Technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Technologies = () =>{
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>HTML</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+export default Technologies;
