bd4217197174eb053cba47ee9ed0b3062022678d
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2022-02-28 16:56:55 +0400

    ignat-hw

--- it-camasutra/ignatHW/my-hw-ignat/src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ it-camasutra/ignatHW/my-hw-ignat/src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -1,8 +1,9 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import React , {ChangeEvent , DetailedHTMLProps , InputHTMLAttributes} from 'react'
 import s from './SuperCheckbox.module.css'
 
+
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement> , HTMLInputElement>
 
 type SuperCheckboxPropsType = DefaultInputPropsType & {
     onChangeChecked?: (checked: boolean) => void
@@ -11,16 +12,31 @@ type SuperCheckboxPropsType = DefaultInputPropsType & {
 
 const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
-        onChange, onChangeChecked,
-        className, spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
+        type , // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        onChange , onChangeChecked ,
+        className , spanClassName ,
+        children , // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
 
         ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // сделайте так чтоб работал onChange и onChangeChecked
+        if (e.currentTarget.checked) {
+            if (onChangeChecked) {
+                onChangeChecked(true)
+            }
+        } else {
+            if (onChangeChecked) {
+                onChangeChecked(false)
+            }
+        }
+        // if (onChangeChecked) {
+        //     onChangeChecked(true)
+        // } else {
+        //     onChangeChecked(false)
+        // }
+
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
